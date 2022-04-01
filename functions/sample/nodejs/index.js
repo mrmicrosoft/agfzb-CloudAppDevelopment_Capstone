62bde1ca54aff4b97522a1da8fc2406a09e19c10
Emmanuel Oben <obentech@gmail.com>
2022-04-01 18:34:43 +0100

    Repo Updated

--- functions/sample/nodejs/index.js
+++ functions/sample/nodejs/index.js
@@ -1,24 +1,85 @@
 /**
- * Get all dealerships
- */
+  *
+  * main() will be run when you invoke this action
+  *
+  * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+  *
+  * @return The output of this action, which must be a JSON object.
+  *
+  */
+ const Cloudant = require('@cloudant/cloudant');
 
-const Cloudant = require('@cloudant/cloudant');
-
-function main(params) {
-
-    const cloudant = Cloudant({
-        url: params.COUCH_URL,
-        plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
-    });
-
-    let dbList = getDbs(cloudant);
-    return { dbs: dbList };
-}
-
-function getDbs(cloudant) {
-    cloudant.db.list().then((body) => {
-        body.forEach((db) => {
-            dbList.push(db);
-        });
-    }).catch((err) => { console.log(err); });
-}
+ function main(params) {
+ 
+     const cloudant = Cloudant({
+         url: params.COUCH_URL,
+         plugins: { iamauth: { iamApiKey: params.IAM_API_KEY } }
+     });
+ 
+     let dealerships = getDealership(cloudant, params);
+     return dealerships;
+ }
+ 
+ function getDealership(cloudant, params) {
+     return new Promise(function (resolve, reject) {
+         const dealershipDb = cloudant.use('dealerships'); //Database to use
+         
+         if (params.state) {
+             // return dealership with this state
+             dealershipDb.find({
+                 "selector": {
+                     "st": {
+                         "$eq": params.state
+                     }
+                 }
+             },function (err, result){
+                 if (err) {
+                     console.log("🚀 ~ function: get-dealership.js ~ line 35 ~ err", err)
+                     reject(err);
+                 }
+                 let code=200; 
+                 if (result.docs.length==0){
+                     code= 404;
+                 } 
+                 resolve({
+                     statusCode: code,
+                     headers: { 'Content-Type': 'application/json' },
+                     body: result
+                 }); 
+             }); 
+         }else if (params.id) {
+             id=parseInt(params.dealerId) 
+             
+             // return dealership with this state 
+             dealershipDb.find({selector: {id:parseInt(params.id)}}, function (err, result) {
+                 if (err){
+                     console.log("🚀 ~ function: get-dealership.js ~ line 54 ~ err", err)
+                     reject(err);
+                 }
+                 let code=200; 
+                 if (result.docs.length==0){
+                     code= 404;
+                 }
+                 resolve({
+                     statusCode: code,
+                     headers: { 'Content-Type': 'application/json' },
+                     body: result
+                 });
+             });
+        }else{
+             // return all documents
+             dealershipDb.list({ include_docs: true }, function (err, result) {
+                 if (err) {
+                     console.log("🚀 ~ function: get-dealership.js ~ line 71 ~ err", err)
+                     reject(err);
+                 }
+                 resolve({
+                     statusCode: 200,
+                     headers: { 'Content-Type': 'application/json' },
+                     body: result
+                 });
+             });
+         } 
+     }); 
+ } 
+ 
--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -7,26 +7,29 @@
 # @return The output of this action, which must be a JSON object.
 #
 #
-from cloudant.client import Cloudant
-from cloudant.error import CloudantException
-import requests
-
+import sys
+from ibmcloudant.cloudant_v1 import CloudantV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 def main(dict):
-    databaseName = "dealerships"
-
+    authenticator = IAMAuthenticator('F7FM6RvKB5x-p7mUNYAvwRTi5FymO9PbhHNWANwkE8wW')
+    service = CloudantV1(authenticator=authenticator)
+    service.set_service_url("https://7048706c-0b51-4bcb-8bec-0e969192e656-bluemix.cloudantnosqldb.appdomain.cloud")
+    
+    response = service.post_find(
+        db='reviews',
+        selector={'dealership': {'$eq': int(dict["dealerId"])}},
+    ).get_result()
+    
     try:
-        client = Cloudant.iam(
-            account_name=dict["COUCH_USERNAME"],
-            api_key=dict["IAM_API_KEY"],
-            connect=True,
-        )
-        print("Databases: {0}".format(client.all_dbs()))
-    except CloudantException as ce:
-        print("unable to connect")
-        return {"error": ce}
-    except (requests.exceptions.RequestException, ConnectionResetError) as err:
-        print("connection error")
-        return {"error": err}
-
-    return {"dbs": client.all_dbs()}
+        # result_by_filter=my_database.get_query_result(selector,raw_result=True)
+        result= {
+            'headers': {'Content-Type':'application/json'},
+            'body': {'data':response}
+        }
+        return result
+    except:
+        return {
+        'statusCode': 404,
+        'message': 'Something went wrong'
+        }
